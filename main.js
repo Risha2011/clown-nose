85924caddfb2e851b387a48ed3698151bfac0cd7
Risha2011 <74859480+Risha2011@users.noreply.github.com>
2021-02-02 15:40:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX = 0;
+noseY = 0;
+
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/gcR0ZpWv/Png-Item-1147898.png')
 }
 
 function setup(){
@@ -14,6 +18,7 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){
@@ -27,7 +32,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
